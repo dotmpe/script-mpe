0b7387461db6d37b1d0b575e14ea52d38e6a2fc0
Berend van Berkum <berend@dotmpe.com>
2009-09-14 15:47:07 +0200

    New and modified tools

--- ANSI-shell-coloring.py
+++ ANSI-shell-coloring.py
@@ -4,6 +4,7 @@ import sys
 s1, s2, s3, s4 = "Foo", "Bar", "Baz", "spam"
 
 
+print "30m",
 print "31m",
 print "32m",
 print "33m",
@@ -11,6 +12,7 @@ print "34m ",
 print "35m ",
 print "36m ",
 print "37m "
+print "\x1b[30m%s\x1b[0m" % (s1),
 print "\x1b[31m%s\x1b[0m" % (s1),
 print "\x1b[32m%s\x1b[0m" % (s2),
 print "\x1b[33m%s\x1b[0m" % (s3),
@@ -18,6 +20,7 @@ print "\x1b[34m%s\x1b[0m" % (s4),
 print "\x1b[35m%s\x1b[0m" % (s4),
 print "\x1b[36m%s\x1b[0m" % (s4),
 print "\x1b[37m%s\x1b[0m" % (s4)
+print "\x1b[1;30m%s\x1b[0m" % (s1),
 print "\x1b[1;31m%s\x1b[0m" % (s1),
 print "\x1b[1;32m%s\x1b[0m" % (s2),
 print "\x1b[1;33m%s\x1b[0m" % (s3),
@@ -25,6 +28,7 @@ print "\x1b[1;34m%s\x1b[0m" % (s4),
 print "\x1b[1;35m%s\x1b[0m" % (s4),
 print "\x1b[1;36m%s\x1b[0m" % (s4),
 print "\x1b[1;37m%s\x1b[0m" % (s4)
+print "\x1b[40m%s\x1b[0m" % (s1),
 print "\x1b[41m%s\x1b[0m" % (s1),
 print "\x1b[42m%s\x1b[0m" % (s2),
 print "\x1b[43m%s\x1b[0m" % (s3),
@@ -32,10 +36,12 @@ print "\x1b[44m%s\x1b[0m" % (s4),
 print "\x1b[45m%s\x1b[0m" % (s4),
 print "\x1b[46m%s\x1b[0m" % (s4),
 print "\x1b[47m%s\x1b[0m" % (s4)
+print "\x1b[1;40m%s\x1b[0m" % (s1),
 print "\x1b[1;41m%s\x1b[0m" % (s1),
 print "\x1b[1;42m%s\x1b[0m" % (s2),
 print "\x1b[1;43m%s\x1b[0m" % (s3)
+print "\x1b[2;40m%s\x1b[0m" % (s1),
 print "\x1b[2;41m%s\x1b[0m" % (s1),
 print "\x1b[2;42m%s\x1b[0m" % (s2)
+print "\x1b[3;40m%s\x1b[0m" % (s1),
 print "\x1b[3;41m%s\x1b[0m" % (s1)
-
--- relink.py
+++ relink.py
@@ -5,47 +5,107 @@ import os
 import sys
 import re
 
-usage = "Usage: % relink match replace [- | links...]"
-noact = False
-if not len(sys.argv)>3:
+usage = """Relink, rename the targets of symbolic links using regular
+expressions.
+
+Usage: % relink [options] match replace [- | links...]
+
+Options:
+	-n  
+		No-act, only print the shell equivalent of the link command that would be 
+		performed
+	-v
+		Verbose.
+	--shell-commands  
+		Same as no-act, but also print the shell equivalent of the unlink command 
+		that is normally performed.		
+"""
+
+seps = ' \n\r\0\t'
+noact, noact_pr_shell = False, False
+if not len(sys.argv)>3 or '-h' in sys.argv or '-?' in sys.argv or '--help' in sys.argv:
 	sys.exit(usage)
 
-if sys.argv[3] == '-':
-	links = sys.stdin.read().split('\0')
-else:
-	links = sys.argv[3:]
-
+opts = 0
 if '-n' in sys.argv:
 	noact = True
+	opts += 1
+if '--shell-commands' in sys.argv:
+	noact = True
+	noact_pr_shell = True
+	opts += 1
 
+verbose = False
+if '-v' in sys.argv:
+	verbose = True
+	opts += 1
 
-find, replace = sys.argv[1:3]
+if sys.argv[-1] == '-':
+	links = sys.stdin.readlines()#.split(seps)
+else:
+	links = sys.argv[opts+3:]
+find, replace = sys.argv[opts+1:opts+3]
 
 find = re.compile(find)
 
 pwd = os.getcwd()
 
+# parse links, 1st pass	
+_links = []	
 for link in links:
-	link = link.strip(' \n\r\0\t')
-	if not link:	
+	link = link.strip(seps)
+	if not link or not os.path.islink(link):	
 		continue
 
-	target = os.readlink(link)
+	link = os.path.abspath(link)	
+	linkdir = os.path.dirname(link)
+	os.chdir(linkdir)
+	target = os.path.abspath(os.readlink(link))
+	_links.append((link, target))
+	os.chdir(pwd)
+links = _links
+
+# rename links, 2nd pass
+for link, target in links:
 	if find.match(target):
+		if verbose:
+			print "Relinking <%s> -> <%s>" % (link, target)
 
 		linkdir = os.path.dirname(link)
-		if linkdir:
-			os.chdir(linkdir)
+		if linkdir and linkdir != os.getcwd():
+			try:
+				if noact_pr_shell:
+					print 'cd "%s"' % linkdir
+				os.chdir(linkdir)
+			except IOError, e:
+				print >>sys.stderr, "Unable to change to directory for link <%s>, skipping rename..." % link
+				continue
 
 		linkname = os.path.basename(link)
 		ntarget = find.sub(replace, target)
 
 		if noact:
-			print 'ln -s ', ntarget, linkname
+			if noact_pr_shell:
+				print 'rm "%s"' % linkname
+			print 'ln -s "%s" "%s"' % (ntarget, linkname)
 		else:
-			os.unlink(linkname)
-			os.symlink(ntarget, linkname)
+			try:
+				os.unlink(linkname)
+			except IOError, e:	
+				print >>sys.stderr, "Unable to unlink <%s>, skipping rename..." % link
+				continue
+			try:
+				os.symlink(ntarget, linkname)
+			except IOError, e:	
+				print >>sys.stderr, "Link <%s> to renamed target <%s> failed!\n!!! Lost link: <%s> -> <%s>" % (link, ntarget, link, target)
+				continue
 
+	elif verbose:
+		print >>sys.stderr, 'Unmatched link target for <%s>, target was <%s>' % (link, target)
+
+if pwd and pwd != os.getcwd():
+	if noact_pr_shell:
+		print 'cd ', pwd 
 	os.chdir(pwd)
 
 
