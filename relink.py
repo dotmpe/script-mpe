697ecf1237ff55b550a781a44e75cb8f89f4ac08
Berend van Berkum <berend@dotmpe.com>
2011-01-15 15:46:02 +0100

    Added more simple rewrite for symlink relink script

--- relink.py
+++ relink.py
@@ -19,7 +19,44 @@ Options:
 	--shell-commands  
 		Same as no-act, but also print the shell equivalent of the unlink command 
 		that is normally performed.		
+
 """
+def relink(link, target, ntarget):
+	global verbose
+
+	if verbose:
+		print "Relinking <%s> (%s)" % (link, target)
+
+	linkdir = os.path.dirname(link)
+	if linkdir and linkdir != os.getcwd():
+		try:
+			if linkdir != os.getcwd():
+				if noact_pr_shell:
+					print 'cd "%s"' % linkdir
+				os.chdir(linkdir)
+		except IOError, e:
+			print >>sys.stderr, "Unable to change to directory for link <%s>, skipping rename..." % link
+			return
+
+	linkname = os.path.basename(link)
+	if noact:
+		if noact_pr_shell:
+			print 'rm "%s"' % linkname
+			print 'ln -s "%s" "%s"' % (ntarget, linkname)
+		else:
+			print "Rewrite target <%s> to <%s>" % (target, ntarget)
+	else:
+		try:
+			os.unlink(linkname)
+		except IOError, e:	
+			print >>sys.stderr, "Unable to unlink <%s>, skipping rename..." % link
+			return
+		try:
+			os.symlink(ntarget, linkname)
+		except IOError, e:	
+			print >>sys.stderr, "Link <%s> to renamed target <%s> failed!\n!!! Lost link <%s> (%s)" % (link, ntarget, link, target)
+			return
+
 
 seps = ' \n\r\0\t'
 noact, noact_pr_shell = False, False
@@ -28,11 +65,11 @@ if not len(sys.argv)>3 or '-h' in sys.argv or '-?' in sys.argv or '--help' in sy
 
 opts = 0
 if '-n' in sys.argv:
-	noact = True
+	noact = True # dry run
 	opts += 1
 if '--shell-commands' in sys.argv:
 	noact = True
-	noact_pr_shell = True
+	noact_pr_shell = True # dry run, print shell command
 	opts += 1
 
 verbose = False
@@ -49,6 +86,8 @@ find, replace = sys.argv[opts+1:opts+3]
 find = re.compile(find)
 
 pwd = os.getcwd()
+if verbose:
+	print "# In directory %s" % pwd
 
 # parse links, 1st pass	
 _links = []	
@@ -68,37 +107,18 @@ links = _links
 # rename links, 2nd pass
 for link, target in links:
 	if find.match(target):
-		if verbose:
-			print "Relinking <%s> -> <%s>" % (link, target)
 
-		linkdir = os.path.dirname(link)
-		if linkdir and linkdir != os.getcwd():
-			try:
-				if noact_pr_shell:
-					print 'cd "%s"' % linkdir
-				os.chdir(linkdir)
-			except IOError, e:
-				print >>sys.stderr, "Unable to change to directory for link <%s>, skipping rename..." % link
-				continue
-
-		linkname = os.path.basename(link)
 		ntarget = find.sub(replace, target)
+		relink(link, target, ntarget)
 
-		if noact:
-			if noact_pr_shell:
-				print 'rm "%s"' % linkname
-			print 'ln -s "%s" "%s"' % (ntarget, linkname)
-		else:
-			try:
-				os.unlink(linkname)
-			except IOError, e:	
-				print >>sys.stderr, "Unable to unlink <%s>, skipping rename..." % link
-				continue
-			try:
-				os.symlink(ntarget, linkname)
-			except IOError, e:	
-				print >>sys.stderr, "Link <%s> to renamed target <%s> failed!\n!!! Lost link: <%s> -> <%s>" % (link, ntarget, link, target)
-				continue
+	elif find.search(target):
+		m = find.search(target)
+		if len(m.groups()) > 1:
+			print >>sys.stderr, "Search pattern should match either single group or entire target path. "
+			continue
+
+		ntarget = find.sub(replace, target, count=1)
+		relink(link, target, ntarget)
 
 	elif verbose:
 		print >>sys.stderr, 'Unmatched link target for <%s>, target was <%s>' % (link, target)
