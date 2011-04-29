a793aca55d86125f4354dbb3cef47d38fb911998
B. van Berkum <dev@dotmpe.com>
2011-04-29 04:42:43 +0200

    Starting to parse existing tag ID's from document.

--- radical.py
+++ radical.py
@@ -93,12 +93,23 @@ indices_comments = Table('indices_comments', Base.metadata,
 class Comment(Base):
     __tablename__ = 'document_embedded_comments'
     id = Column(Integer(11), primary_key=True)
-    filename = Column(String(255), index=True, unique=True)
+    filename = Column(String(255), index=True)
     pattern = Column(String(255))
+    # XXX: unique on filename/linenumber?
     last_seen_linenumber = Column(Integer(11))
     tags = relationship(Index, secondary=indices_comments, backref='comments')
     comment = Column(Text, index=True)
 
+
+# Storage & service IO
+def store_comments(session, data, comment_span, flavour_spec, tagname, matchbox):
+    """
+    """
+    comment_data = data[slice(*comment_span)]
+    
+    pass
+
+# Content data scanning
 
 def at_line(offset, width, data):
     #data = data.decode('utf-8')
@@ -113,48 +124,22 @@ def at_line(offset, width, data):
 "Span is beyond document bounds: %r while document bytes:%r %r, lines:%s " % \
 ((offset, width), chars, len(data), len(lines))
 
+collapse_ws = re.compile('\s*').sub
 
-def get_tagged_comment(offset, width, data, language_keys=('unix_generic','c', 'c_line')):
+def get_tagged_comment(offset, width, data, language_scan):
     """
     Return the comment span that has a tag embedded at offset/width.
-
-    The comment runs from the end of the tag, to the next '.' followed by
-    whitespace, or the end of the line. 
-    
-    This span may include metacharacters used to markup comments. Recognized
-    are:
-
-    - Unix line comments (starting with '#', optionally whitespace prefixed).
-    - FIXME: C-style line and block comments.
-
+    This scans for continues sequences of comment lines, and may return spans
+    for comment blocks that contain other tags. Also, this span may include 
+    metacharacters used to markup comments. Further parsing of the comment
+    block is left to the caller.
     """
     tag_line, line_offset, line_width = at_line(offset, width, data)
 
     lines = data.split('\n')
 
-    # TODO: groups of filetype tags for each flavour scanned comment
-    comment_flavours = {
-            'unix_generic': (),
-            'c':   ('c', 'js', 'hx', 'php', ),
-            'c_line':   ('c', 'js', 'hx', 'php', ),
-            'vim': ('vim', 'vimrc'),
-            'rst': ('rst', 'text',),
-            'sql': ('sql', 'mysql',),
-            'ini':  ('ini',),
-            'py':  ('py',),
-            # XXX: collapse htm and html
-            'xml': ('xml', 'xslt', 'xsd', 'relax', 'xhtml', 'html'),
-            'sgml': ('sgml','html'),
-        }
-    # Start and end regex patterns per comment flavour
-    language_markup = {
-            'unix_generic': ('^#.*$',),
-            'c': ('^\s*(\/\*).*$','^.*(\*\/)\s*$'),
-            'c_line': ('^\s*\/\/.*$',),
-        }
-
-    for language_key in language_keys:
-        comment_scan = language_markup[language_key]
+    for language_key in language_scan:
+        comment_scan = language_scan[language_key]
         match_start = re.compile(comment_scan[0], re.M).match
 
         if not match_start(lines[tag_line]):
@@ -190,36 +175,85 @@ def get_service(t):
     __import__('radical_'+t)
     pass
 
-def find(session, tags, matchbox, paths):
+def find(session, tags, matchbox, data, comment_flavours):
+    """
+    Look for tags in data, using the compiled tag regexes
+    in matchbox, and post processing each found flavour of comment block to the
+    distinct tagged comments.
+
+    The tagged comment body text runs from the end of the tag, to the next '.' 
+    followed by whitespace, or the end of the line for embedded comments.
+    Comment blocks starting with a tag include the body text up to the end of
+    the comment block. This makes a distinction between tagged comment lines and
+    blocks.
+    
+    Recognized
+    are:
+
+    - Unix line comments (starting with '#', optionally whitespace prefixed).
+    - FIXME: C-style line and block comments.
+    """
+    for tagname in matchbox:
+
+        for match in matchbox[tagname].finditer(data):
+            tag_span = match.start(), match.end()
+
+            # Get entire comment
+            comment = get_tagged_comment(tag_span[0],
+                    tag_span[1]-tag_span[0], data,
+                    comment_flavours)
+            if not comment: 
+                continue
+            comment_flavour, comment_span, lines = comment
+            #(comment_start, comment_end), (start_line, end_line) = comment_span, lines
+            comment_data = data[slice(*comment_span)]
+
+            # Tracked or anonymous tags
+            #print tag_span, tagname, data[slice(*tag_span)], match.groups()
+            if tags[tagname]:
+                current_id = None
+                if match.group(2):
+                    assert tagname in tags, "Need index type for tracked tag at %s" % (tag_span,)
+                    current_id = match.group(2)
+                if current_id:
+                    print 'Existing:', tagname, current_id, tag_span, comment_data
+                    pass
+                else:
+                    print 'New:', tagname, tag_span, comment_data
+                    pass # Identify new tracked tagged comment
+                    # Write ID to file
+            else:
+                print 'Anonymous:', tagname, tag_span, comment_data
+                pass # Anonymous tag
+
+            style_regexes = comment_flavours[comment_flavour]
+            continue
+            # TODO: store
+            for tagged in store_comments(session, data, comment_span, 
+                    style_regexes, tagname, matchbox[tagname]):
+                storage = None
+                if tags[tagname]:
+                    id_pattern, id_fmt, id_generator = tags[tagname]
+                    storage = get_service(id_generator)
+
+
+def find_files(session, tags, matchbox, paths, comment_flavours):
+    """
+    Look for tags in the data at each file path.
+    """
+
     for p in paths:
         if not os.path.exists(p):
             err("Path does not exist: %s", p)
         elif os.path.isdir(p):
             subs = [ os.path.join(p, d) for d in os.listdir(p) ]
-            find(session, tags, matchbox, subs)
+            find_files(session, tags, matchbox, subs, comment_flavours)
         elif not os.path.isfile(p):
             err("Ignored non-file path: %s", p)
         else:
             data = open(p).read()
-            for tagname in matchbox:
-
-                storage = None
-                if tags[tagname]:
-                    id_fmt, id_generator = tags[tagname]
-                    storage = get_service(id_generator)
-
-                # TODO
-                for match in matchbox[tagname].finditer(data):
-                    start, end = match.start(), match.end()
-                    comment = get_tagged_comment(start, end-start, data)
-                    if not comment: 
-                        continue
-                    comment_flavour, span, lines = comment
-                    (comment_start, comment_end), (start_line, end_line) = span, lines
-                    print p, comment_flavour, data[comment_start:comment_end]
-                    current_id = None
-                    #print '\t',t,m
-
+            find(session, tags, matchbox, data, comment_flavours)
+                    
 # 
 
 def get_session(dbref):
@@ -235,23 +269,51 @@ def err(msg, *args):
 
 def main():
     paths = sys.argv[1:]
+ 
+    dbref = 'sqlite:////radical.sqlite';
 
+    # TODO: groups of filetype tags for each flavour scanned comment
+    # XXX: probably use gate to map between content-type and format tag
+    #flavour_format_map = {
+    #        'unix_generic': (),
+    #        'c':   ('c', 'js', 'hx', 'php', ),
+    #        'c_line':   ('c', 'js', 'hx', 'php', ),
+    #        'vim': ('vim', 'vimrc'),
+    #        'rst': ('rst', 'text',),
+    #        'sql': ('sql', 'mysql',),
+    #        'ini':  ('ini',),
+    #        'py':  ('py',),
+    #        # XXX: collapse htm and html
+    #        'xml': ('xml', 'xslt', 'xsd', 'relax', 'xhtml', 'html'),
+    #        'sgml': ('sgml','html'),
+    #    }
+
+    # Start/end regex patterns per comment flavour
+    comment_flavours = {
+            'unix_generic': ('^(\s*#).*$',),
+            'c': ('^(\s*\/\*).*$','^.*(\*\/\s*)$'),
+            'c_line': ('^(\s*\/\/).*$',),
+        }
+    # Tag pattern, format and index type
     tags = {
-        'TODO': ('%(tagname)s:%(numid)i', 'numeric_index'),
+        'TODO': ('(%s)\:?(?:([0-9]+)\:)?', '%(tagname)s:%(numid)i', 'numeric_index'),
 #        'FIXME': ('%(tagname)s:%(id)s', 'tiny_ticket'),
         'XXX': None,
 #        'FACIOCRM': ('%(tagname)s-%(id)i', 'atlassian_jira'),
     }
 
-    dbref = 'sqlite:////radical.sqlite';
-
     matchbox = {}
     for tagname in tags:
-        matchbox[tagname] = re.compile(r"(%s)" % tagname)
+        pattern = r"(%s)" % tagname
+        if tags[tagname]:
+            pattern = tags[tagname][0] % tagname
+        matchbox[tagname] = re.compile(pattern)
 
     dbsession = get_session(dbref)
-    find(dbsession, tags, matchbox, paths) 
+    find_files(dbsession, tags, matchbox, paths, comment_flavours) 
 
 
 if __name__ == '__main__':
     main()
+
+# vim:et:
