e0e3de75629219d092e4c665c7362ff396de40d3
B. van Berkum <berend@brixcrm.nl>
2011-11-12 21:02:40 +0100

    Adding updated edit.sh

--- edit.sh
+++ edit.sh
@@ -1,13 +1,63 @@
 #!/bin/bash
 origin=$(git remote -v|grep origin|grep fetch|sed -e 's/^origin.\(.*\)..fetch./\1/g')
 echo 'Origin:' $origin
-git fetch origin master
-git diff HEAD -- $1
-( 
-    [ "$(git status $1 | grep Untracked)" ] \
-    && echo New file: $1
-) || (
-    [ "$(git status $1 | grep Modified)" ] \
-    && echo Modified file: $1
-)
+[ "$origin" ] || ( echo Need to work from GIT checkout. && exit 2 )
+
+function update-git()
+{
+    echo In update
+    git status
+    [ "$(git status|grep '(added\|modified\|deleted):')" ] && (
+        echo "Adding" \
+        && git add --interactive \
+        && git commit; \
+        return 1
+    ) || ( \
+        echo "Whut?"
+        [ "$(git status|grep 'On branch master')" ] || (
+            git checkout master && update
+        )
+        return 1
+    ) || \
+        return 0
+}
+function edit()
+{
+    ( 
+        [ ! -f "$1" ] \
+        && touch $1 \
+        && ( \
+            [ "$(git status $1 | grep Untracked)" ] \
+            && echo Adding new file: $1 \
+            && git add $1
+        ) || ( \
+            echo "Error creating $1"
+        )
+    ) || (
+        [ "$(git status $1 | grep -i modified)" ] \
+        && echo Modified file: $1
+    ) || (
+        echo git status $1
+    )
+    #git diff HEAD --no-ext-diff -- $1
+}
+function commit()
+{
+    echo git add $1
+    echo git commit
+    echo git push origin master
+}
+echo "Calling update"
+update-git
+dirty=$!
+while [ $dirty ];
+do 
+    echo Dirty
+    update-git
+    dirty=$!
+done
+#update $1
+#$EDITOR $1
+#update $1
+#commit $1
 
