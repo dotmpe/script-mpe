15d5668350d37f2158805ee82729e0fd47874b2e
B. van Berkum <berend@brixcrm.nl>
2011-11-12 21:16:09 +0100

    -

--- edit.sh
+++ edit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 origin=$(git remote -v|grep origin|grep fetch|sed -e 's/^origin.\(.*\)..fetch./\1/g')
 echo 'Origin:' $origin
-[ -z "$origin" ] && ( echo Need to work from GIT checkout. && exit 2 )
-[ -z "$EDITOR" ] && ( echo Editor environment not set. && exit 3 )
+[ "$origin" ] || ( echo Need to work from GIT checkout. && exit 2 )
+[ "$EDITOR" ] || ( echo Editor environment not set. && exit 3 )
 
 function update-git()
 {
