39d8aa846573ecede4f8aee7fb20f69e31e3a795
B. van Berkum <berend@brixcrm.nl>
2012-02-18 12:20:00 +0100

    Adding snake changes (IoC review)

--- rsr_snake.py
+++ rsr_snake.py
@@ -1,36 +1,60 @@
+import sys
+sys.path.append('/Library/Python/2.6/site-packages/snake_guice-0.3-py2.6.egg')
+
+# Domain objects, using injection where required
+from snakeguice import inject, annotate
+
+
+class IBaz(object):
+    pass
+
+class IBar(object):
+    """This will be used an the interface when creating a binding."""
+
+class SmallBlockBar(object):
+    """Implementation of a small block bar."""
+
+class Foo(object):
+    """An implementation of a car.""" 
+
+    @inject(bar=IBar, foo=IBaz)
+    @annotate(foo="test_foo_8325")
+    def __init__(self, bar, foo):
+        self.bar = bar
 
 class Resourcer(object):
     pass
 
-class Module:
+
+# Module
+
+class RightDoor: pass
+class SmallBlockBar: pass
+class SnakeModule(object):
     def configure(self, binder):
-        binder.bind(ICommand, to=Resourcer)
+        binder.bind(IBar, to=SmallBlockBar)
+        binder.bind(IDoor, annotated_with='test_foo_8325', to=RightDoor)
 
-class Command(object):
-    def run(self, argv):
-        injector = Injector(Module())
-        injector.get_instance(Resources)
-
-from snakeguice.modules import ConfigModule
-from snakeguice.interfaces import Config
-
-from snakeguice import inject, Injected, Config
-
-class MyLogger(object):
-
-    @inject(filename=Config('app.cfg:logger:filename'),
-            loglevel=Config('app.cfg:logger:loglevel'))
-    def __init__(self, filename,loglevel):
-        pass
+        #binder.bind(ICommand, to=Resourcer)
 
 
-from myapp import MyConfigAdapter
+# Main
 
-class MyConfigModule(ConfigModule):
+from snakeguice import Injector
 
-    paths = ['/path/to/config/dir']
 
-    def configure(self, binder):
-        binder.bind(Config, to=MyConfigAdapter)
+def main(args):
+    injector = Injector(SnakeModule())
+    car = injector.get_instance(Foo) 
+   
+    print mod
+    print injector
+    print car, car.bar
+    
+    assert isinstance(car, Foo)
+    assert isinstance(car.bar, SmallBlockBar)
 
+
+if __name__ == '__main__':
+    main([])
 
