8ee2a45d2580ecf66f9210f556ed62c5219aa63a
B. van Berkum <dev@dotmpe.com>
2012-11-22 23:34:17 +0100

    Transmission plugins

--- munin/transmissionbt.sh
+++ munin/transmissionbt.sh
@@ -4,28 +4,13 @@
 # rateUpload
 # rateDownload
 # percentDone
-function get_file() # field 
+function get_file() # method field 
 {
-	TMPF=/tmp/transmissionbt-munin-$1-stats
-
-	if test -e $TMPF
-	then
-		OLD=$(stat -c %Z $TMPF)
-		NOW=$(date +%s)
-		AGEMINUTES=$(( ( $NOW - $OLD ) / 60 ))
-		if [ $AGEMINUTES -ge 4 ];
-		then
-			rm $TMPF
-		fi
-	fi
-
-	if test ! -e $TMPF
-	then
-		echo '{"method":"torrent-get","tag":1,"arguments":{"fields":["'$1'"]}}' \
-			| transmission-remote -n mpe:tr4bt0 -l \
-			> $TMPF
-	fi
-
-	cat $TMPF
+	#TMPF=/tmp/transmissionbt-munin-$1-stats
+	case $1 in
+		torrent-get )
+			transmission-remote -n mpe:tr4bt0 -l 
+			;;
+	esac
 }
 
--- munin/transmissionbt_rates.sh
+++ munin/transmissionbt_rates.sh
@@ -7,24 +7,9 @@ case $1 in
 graph_title Transmission up/down rate
 graph_vlabel Rate (KB/s)
 graph_category Transmission
-downRate.label Download rate
-upRate.label Upload rate
+downrate.label Download rate
+uprate.label Upload rate
 EOM
-        exit 0;;
-esac
-
-case $1 in
-   config)
-        cat <<'EOM'
-graph_title Total number of torrents
-graph_vlabel Count
-graph_category transmission
-EOM
-		for T in   upRate downRate
-		do
-			echo -n "$T.label " | tr 'A-Z' 'a-z'
-			echo $T 
-		done
         exit 0;;
 esac
 
