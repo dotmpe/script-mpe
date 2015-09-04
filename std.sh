95b162c9e4a79a06c472de72536c4dbded2428e2
B. van Berkum <dev@dotmpe.com>
2015-09-04 04:24:01 +0200

    Syntax. Testing colors at Darwin

--- std.sh
+++ std.sh
@@ -3,29 +3,29 @@
 # stdio/stderr/exit util
 log()
 {
-	[ -n "$(echo "$*")" ] || return 1;
-	echo "[$scriptname.sh:$cmd] $1"
-	#[ $level -le $VERBOSE ] && echo "$1"
+  [ -n "$(echo "$*")" ] || return 1;
+  echo "[$scriptname.sh:$cmd] $1"
+  #[ $level -le $VERBOSE ] && echo "$1"
 }
 err()
 {
-	case "$(echo $1 | tr 'A-Z' 'a-z')" in
-		warn*|err*|notice ) log "$1: $2" 1>&2 ;;
-		* ) log "$2" 1>&2 ;;
-	esac
-	[ -z $3 ] || exit $3
+  case "$(echo $1 | tr 'A-Z' 'a-z')" in
+    warn*|err*|notice ) log "$1: $2" 1>&2 ;;
+    * ) log "$2" 1>&2 ;;
+  esac
+  [ -z $3 ] || exit $3
 }
 error()
 {
-	err "Error" "$1" "$2"
+  err "Error" "$1" "$2"
 }
 warn()
 {
-	err "Warning" "$1" "$2"
+  err "Warning" "$1" "$2"
 }
 note()
 {
-	err "Notice" "$1" "$2"
+  err "Notice" "$1" "$2"
 }
 info()
 {
