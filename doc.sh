995651aa95e9f18a3d7dfed0f317e5b43c389218
B. van Berkum <dev@dotmpe.com>
2015-09-05 12:50:05 +0200

    Minor updates for htd

--- doc.sh
+++ doc.sh
@@ -13,21 +13,42 @@ doc_name_arg()
   }
 }
 
-doc_find()
+doc_list_all()
 {
-  # XXX: write a routine to detect dt, or emph tags
-  files=$(grep -li '^'$name'$' $HOME/htdocs/${dir}/*.rst)
-  test -z "$files" && {
-      grep -li '\*'$name'\*' $HOME/htdocs/${dir}/*.rst
-      files=$(grep -li '\*'$name'\*' $HOME/htdocs/${dir}/*.rst)
-      test -n "$files" || return 1
+  ls $dir/$name.* 2> /dev/null && return
+  grep -sni '^'$name'$' $HOME/htdocs/${dir}/*.rst && return
+  grep -sni '\*'$name'\*' $HOME/htdocs/${dir}/*.rst && return
+  note "no results"
+  return 1
+}
 
+doc_try_fs()
+{
+  ls $dir/$name.* &> /dev/null || return
+  files=$(echo $dir/$name.*)
+  file_cnt="$(echo $(echo $files | wc -w))"
+}
+
+doc_get_first()
+{
+  # ... detect dt, or emph tags
+  files=$(grep -sli '^'$name'$' $HOME/htdocs/${dir}/*.rst)
+  test -z "$files" && {
+      files=$(grep -sli '\*'$name'\*' $HOME/htdocs/${dir}/*.rst)
+      test -n "$files" || return 1
   }
   file_cnt="$(echo $(echo $files | wc -w))"
-  test $file_cnt -ge 0 || return 3
-  test $file_cnt -eq 1 && file=$files || file="$(echo $files | sed 's/^\([^\ ]*\).*$/\1/')"
+  test $file_cnt -gt 0 || return 1
+}
 
+doc_find()
+{
+  doc_try_fs || doc_get_first || return 1
+  test $file_cnt -eq 1 \
+      && file=$files || \
+      file="$(echo $files | sed 's/^\([^\ ]*\).*$/\1/')"
   test -e "$file" \
       || err '' "${bwhite}No doc for ${blue}$dir ${white}/ ${grn}$name${bwhite}. Stop." 1
   info "Found $file_cnt documents, opening first: $file"
 }
+
