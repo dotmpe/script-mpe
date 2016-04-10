8d6b2d82d1871c059de0506c0c1656fb82d10989
B. van Berkum <dev@dotmpe.com>
2016-04-10 04:24:55 +0200

    Testing travis build

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -26,7 +26,7 @@ case "$ENV" in
       # XXX: Skip build on git-annex branches
       test -n "$TRAVIS_COMMIT" || GIT_CHECKOUT=$TRAVIS_COMMIT
       GIT_CHECKOUT=$(git log --pretty=oneline | head -n 1 | cut -f 1 -d ' ')
-      BRANCH_NAMES="$(git ls-remote origin | grep -F $GIT_CHECKOUT | awk '{print "$2\n"}' | xargs basename)"
+      BRANCH_NAMES="$(git ls-remote origin | grep -F $GIT_CHECKOUT | awk '{print $2}' | xargs basename)"
       echo "Branch Names: $BRANCH_NAMES"
       case "$BRANCH_NAMES" in "*annex*" ) exit 0 ;; esac
 
