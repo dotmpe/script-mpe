145994110f7bd512b0c0c036183f4929042817d8
B. van Berkum <dev@dotmpe.com>
2016-05-30 12:31:05 +0200

    Fixes on statusdir

--- statusdir.sh
+++ statusdir.sh
@@ -8,11 +8,18 @@ statusdir__source=$_
 
 set -e
 
+
 statusdir_load()
 {
   [ -z "$STATUSDIR_ROOT" ] && {
       STATUSDIR_ROOT="$(echo ~/.statusdir/)"
       #export STATUSDIR_ROOT
+  }
+  test -n "$be" || { which membash && be=membash; }
+  test -n "$be" || be=fsdir
+
+  test ! -e "$scriptdir/statusdir_$be.sh" || {
+    . $scriptdir/statusdir_$be.sh
   }
 }
 
@@ -136,13 +143,6 @@ statusdir__cons_json()
   mv /tmp/new-status.json $status_json
 }
 
-
-test -n "$be" || {
-  which membash && be=membash
-}
-test -n "$be" || {
-  . $scriptdir/statusdir_$be.sh
-}
 
 
 statusdir__get()
