aa00573bfecbb6e0ea5cb2a9700bde23d1043adf
B. van Berkum <dev@dotmpe.com>
2017-01-14 21:28:54 +0100

    Further cleaning Travis/CI build

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -1,124 +1,81 @@
 #!/bin/sh
 
-set -ex
+. ./tools/sh/env.sh
+. ./util.sh
+. ./main.lib.sh
 
 # entry-point for CI build phase
 echo "entry-point for CI build phase"
 
 
-# FIXME: "Something wrong with pd/std__help"
-#projectdir.sh help
-
-export PATH=$PATH:/usr/local/bin
-#export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages
-export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/site-packages
-export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
-
-
-jsotk.py from-args foo=bar
-jsotk.py objectpath \
-      $HOME/bin/test/var/jsotk/2.yaml \
-      '$.*[@.main is not None]'
-
-# TODO add local tests
-#htd script
-htd tools
-htd install json-spec
-
-#./gtasks
-
-
-. ./tools/sh/env.sh
-
-
 # Start build per env
+test -n "$1" || . ./tools/ci/auto-targets.sh
 
-test -n "$TRAVIS_COMMIT" || GIT_CHECKOUT=$TRAVIS_COMMIT
-GIT_CHECKOUT=$(git log --pretty=oneline | head -n 1 | cut -f 1 -d ' ')
-BRANCH_NAMES="$(echo $(git ls-remote origin | grep -F $GIT_CHECKOUT \
-        | sed 's/.*\/\([^/]*\)$/\1/g' | sort -u ))"
+while test -n "$1"
+do
+  case "$1" in
 
-echo "Branch Names: $BRANCH_NAMES"
-case "$BRANCH_NAMES" in
-  # NOTE: Skip build on git-annex branches
-  *annex* ) exit 0
-    ;;
-  gh-pages )
-      ENV=jekyll
-    ;;
-esac
+     production )
+        # XXX: work in progress. project has only dev or ENV= builds
+        #./configure.sh /usr/local && sudo ENV=$ENV ./install.sh && make test build
+        DESCRIBE="$(git describe --tags)"
+        grep '^'$DESCRIBE'$' ChangeLog.rst && {
+          echo "TODO: get log, tag"
+          exit 1
+        } || {
+          echo "Not a release: missing change-log entry $DESCRIBE: grep $DESCRIBE ChangeLog.rst)"
+        }
+      ;;
 
+    test* )
+        #./configure.sh && make build test
+        . ./tools/ci/test.sh
 
-case "$ENV" in
+        # XXX: cleanup, verify exit of above script (everything again):
+        bats ./test/*-spec.bats
+        ./bin/behat --tags '~@todo&&~@skip'
+      ;;
 
-   production )
-      # XXX: work in progress. project has only dev or ENV= builds
-      #./configure.sh /usr/local && sudo ENV=$ENV ./install.sh && make test build
-      DESCRIBE="$(git describe --tags)"
-      grep '^'$DESCRIBE'$' ChangeLog.rst && {
-        echo "TODO: get log, tag"
-        exit 1
-      } || {
-        echo "Not a release: missing change-log entry $DESCRIBE: grep $DESCRIBE ChangeLog.rst)"
-      }
-    ;;
+    dev )
+        main_debug
 
-  test* )
+        #./gtasks
 
-      #./configure.sh && make build test
-      . ./tools/ci/test.sh
+        #./box-instance x foo bar
+        #./box-instance y
 
-      # XXX: cleanup, verify exit of above script (everything again):
-      bats ./test/*-spec.bats
-      ./bin/behat --tags '~@todo&&~@skip'
-    ;;
+        #./match.sh help
+        #./match.sh -h
+        #./match.sh -h help
+        #./match.sh -s var-names
 
-  dev )
+        #bats
+        ./projectdir.sh test bats-specs bats
+        #( test -n "$PREFIX" && ( ./configure.sh $PREFIX && ENV=$ENV ./install.sh ) || printf "" ) && make test
 
-      echo "TRAVIS_SKIP=$TRAVIS_SKIP"
-      echo "ENV=$ENV"
-      echo "Build dir: $(pwd)"
+        #./matchbox.py help
+        #./libcmd_stacked.py -h
+        #./radical.py --help
+        #./radical.py -vv -h
 
-      . ./util.sh
-      . ./main.lib.sh
-      main_debug
+        ./matchbox.py
 
-      #./box-instance x foo bar
-      #./box-instance y
+        ./basename-reg --help
+        #./basename-reg ffnnec.py
+        #./mimereg ffnenc.py
+      ;;
 
-      #./match.sh help
-      #./match.sh -h
-      #./match.sh -h help
-      #./match.sh -s var-names
-#
-      #bats
-      ./projectdir.sh test bats-specs bats
-      #( test -n "$PREFIX" && ( ./configure.sh $PREFIX && ENV=$ENV ./install.sh ) || printf "" ) && make test
+    jekyll )
+        bundle exec jekyll build
+      ;;
 
-      #./matchbox.py help
-      #./libcmd_stacked.py -h
-      #./radical.py --help
-      #./radical.py -vv -h
+    * )
+        error "Unknown build '$1'" 1
+      ;;
 
-      ./matchbox.py
+  esac
 
-      ./basename-reg --help
-      #./basename-reg ffnnec.py
-      #./mimereg ffnenc.py
-    ;;
+  note "Build '$1' done"
 
-  jekyll )
-      bundle exec jekyll build
-    ;;
-
-  * )
-      echo "Build Env error"
-      env
-
-      echo "Unknown ENV '$ENV' (commit $TRAVIS_COMMIT, branches $BRANCH_NAMES)"
-      exit 1
-    ;;
-
-esac
-
-
+  shift 1
+done
--- tools/ci/pre-run.sh
+++ tools/ci/pre-run.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-set -ex
+. ./tools/sh/env.sh
 
 # entry-point for CI pre-test phase, to do preflight checks, some verbose debugging
 echo "entry-point for CI pre-test phase"
+
 
 whoami
 hostname
 
 echo "*PATH* env:"
 env | grep PATH
+
+echo "TRAVIS_SKIP=$TRAVIS_SKIP"
+echo "ENV=$ENV"
+echo "Build dir: $(pwd)"
 
 
 echo "Pre-flight check.."
@@ -24,4 +29,16 @@ vc.sh help
 radical.py --help
 jsotk.py -h
 htd help
+
+# FIXME: "Something wrong with pd/std__help"
+#projectdir.sh help
+
+jsotk.py from-args foo=bar
+jsotk.py objectpath \
+      $HOME/bin/test/var/jsotk/2.yaml \
+      '$.*[@.main is not None]'
+
+#htd script
+htd tools
+htd install json-spec
 
