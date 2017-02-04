b62b0b2cd0ff95d5a39078b5101d70088a0f829d
Travis <dev+travis@dotmpe.com>
2017-02-04 06:17:57 +0100

    Travis

--- tools/ci/check.sh
+++ tools/ci/check.sh
@@ -29,30 +29,49 @@ composer --version
 behat --version
 bats --version
 realpath --version
-basher version
+basher help
 
 cpan install XML::Generator
 ~/.basher/bin/tap-to-junit-xml --help
 tap-to-junit-xml --help
 
 # Local commands should be on PATH and working OK
-note "Box:" && box version && box -V
-note "Box:" && box help && box -h && box -h stat
-note "Htd:" && htd version && htd -V
-note "Htd:" && htd help && htd -h && htd -h help
-note "Vc:" && vc.sh version
-note "Vc:" && vc.sh help
-note "Rad:" && radical.py --version
-note "Rad:" && radical.py --help
-note "Rad:" && radical.py --help && radical.py -vv -h
-note "jsotk.py:" && jsotk.py version && jsotk.py -V
-note "jsotk.py:" && jsotk.py --help
-note "sh-switch" && sh_switch.py -V
-note "sh-switch" && sh_switch.py --help
-note "match" && match.sh help && match.sh -h && match.sh -h help
-note "matchbox" && matchbox.py help
-note "libcmd-stacked" && libcmd_stacked.py -h
-note "basename-reg" && basename-reg --help
+note "Box"
+box version && box -V
+box help && box -h && box -h stat
+
+note "Htd:"
+htd version && htd -V
+htd help && htd -h && htd -h help
+
+note "Vc:"
+vc.sh version
+vc.sh help
+
+note "Rad:"
+radical.py --version
+radical.py --help
+radical.py --help && radical.py -vv -h
+
+note "jsotk.py:"
+jsotk.py version && jsotk.py -V
+jsotk.py --help
+
+note "sh-switch"
+sh_switch.py -V
+sh_switch.py --help
+
+note "match"
+match.sh help && match.sh -h && match.sh -h help
+
+note "matchbox"
+matchbox.py help
+
+note "libcmd-stacked"
+libcmd_stacked.py -h
+
+note "basename-reg"
+basename-reg --help
 
 # Local scripts should be the same, but anyway try a few..
 ./box version
