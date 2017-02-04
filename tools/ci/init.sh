1e6338fbe91cac4554d58640edfade5168325522
Travis <dev+travis@dotmpe.com>
2017-02-04 04:59:11 +0100

    Travis

--- tools/ci/init.sh
+++ tools/ci/init.sh
@@ -10,6 +10,10 @@ note "entry-point for CI pre-install / init phase"
 # Basicly if these don't run dont bother with anything,
 # But cannot abort/skip a Travis build without failure, can they?
 
+pwd && pwd -P
+whoami
+env | grep -i 'shippable\|travis\|ci'
+
 # This is also like the classic software ./configure.sh stage.
 
 mkdir -vp ~/.local/{bin,lib,share}
