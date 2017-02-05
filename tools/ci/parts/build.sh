03d872c1a5b4b3f15274e7507d032a6ce96a0be4
Travis <dev+travis@dotmpe.com>
2017-02-05 17:38:32 +0100

    Testing

--- tools/ci/parts/build.sh
+++ tools/ci/parts/build.sh
@@ -71,19 +71,16 @@ do case "$BUILD_STEP" in
         failed=build/test-results-failed.list
 
         test -n "$TEST_RESULTS" || TEST_RESULTS=build/test-results-speqs.tap
-        #SUITE="$REQ_SPECS" test_shell $TEST_SHELL $(which bats)
         (
-          SUITE="$REQ_SPECS" test_shell > $TEST_RESULTS
+          SUITE="$REQ_SPECS" test_shell $TEST_SHELL $(which bats)
+          #SUITE="$REQ_SPECS" test_shell > $TEST_RESULTS
         ) || noop
-        wc -l $TEST_RESULTS
 
         test "$SHIPPABLE" != "true" || {
-          which tap-to-junit-xml
-          echo $(which tap-to-junit-xml) --input $TEST_RESULTS \
-            --output $(basepath $TEST_RESULTS .tap .xml)
           perl $(which tap-to-junit-xml) --input $TEST_RESULTS \
             --output $(basepath $TEST_RESULTS .tap .xml)
         }
+        wc -l $TEST_RESULTS $(basepath $TEST_RESULTS .tap .xml)
 
         ## Other tests
         #failed=build/test-results-dev.list
@@ -120,7 +117,6 @@ do case "$BUILD_STEP" in
   esac
 
   note "Step '$BUILD_STEP' done"
-  shift 1
 done
 
 note "Done"
