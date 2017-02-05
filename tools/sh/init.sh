6defb9f5282d0460c8b714102fd831685a7cae90
B. van Berkum <dev@dotmpe.com>
2017-02-05 02:50:17 +0100

    Sh

--- tools/sh/init.sh
+++ tools/sh/init.sh
@@ -7,12 +7,20 @@ test -n "$scriptdir" || export scriptdir="$(pwd -P)"
 # if not provided, auto-setup env
 # assuming execution starts in script dir (project root)
 test -n "$SCRIPTPATH" || {
+
   test -n "$LIB" || {
-    test -n "$scriptdir" \
-      && LIB=$scriptdir \
-      || LIB=$(cd $(dirname $(dirname $0)); pwd -P )
+    test -n "$scriptdir" &&
+      LIB=$scriptdir ||
+      LIB=$(cd $(dirname $(dirname $0)); pwd -P )
     export LIB
   }
+  #test -n "$LIB" || export LIB="$( test -n "$scriptdir" &&
+  #    echo $scriptdir || ( cd $(dirname $(dirname $0)); pwd -P ))"
+
+	#export SCRIPTPATH="$( case "$LIB" in /* ) echo $LIB ;; * )
+  #    echo $(cd "$LIB"; pwd -P)
+  #  ;; esac )"
+
 	SCRIPTPATH=$LIB
 	# get absolute path for scripts lib dir if LIB is relative
 	case "$LIB" in /* ) ;; * )
