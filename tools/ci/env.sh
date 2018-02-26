80933144a088432843f3b670a57fc52c60c3c9fe
B. van Berkum <dev@dotmpe.com>
2018-02-27 00:01:30 +0100

    Testing

--- tools/ci/env.sh
+++ tools/ci/env.sh
@@ -8,7 +8,7 @@ lib_load std str sys bash projectenv env-deps
 
 test -n "$BASH_SH" || error "Need to know shell dist" 1
 test 0 -eq $BASH_SH && {
-    export SCR_SYS_SH=sh || export SCR_SYS_SH=bash-sh
+   export SCR_SYS_SH=sh || export SCR_SYS_SH=bash-sh
 }
 
 . ./tools/sh/env.sh
