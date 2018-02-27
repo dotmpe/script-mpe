1e90bdbbb7df4ccd22de91734ef6794252c30265
B. van Berkum <dev@dotmpe.com>
2018-02-27 01:19:48 +0100

    Testing

--- tools/ci/env.sh
+++ tools/ci/env.sh
@@ -1,10 +1,7 @@
 # Boilerplate env for CI scripts
-
-echo "0.1"
+export DEBUG=1
 . ./tools/sh/init.sh
-echo "0.2"
 lib_load std str sys bash projectenv env-deps
-echo "0.3"
 test -n "$BASH_SH" || error "Need to know shell dist" 1
 test 0 -eq $BASH_SH && {
    export SCR_SYS_SH=sh || export SCR_SYS_SH=bash-sh
