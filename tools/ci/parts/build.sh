85932e8777fccb3528ea2bf26f52f6f39ac1f1d9
B. van Berkum <dev@dotmpe.com>
2018-02-27 02:08:47 +0100

    Testing

--- tools/ci/parts/build.sh
+++ tools/ci/parts/build.sh
@@ -57,6 +57,7 @@ do case "$BUILD_STEP" in
         (
           test_shell $(which bats) &&
           note "Bats shell tests done" &&
+          note "$TEST_FEATURE '$BUSINESS_SUITE'"  &&
           $TEST_FEATURE $BUSINESS_SUITE  &&
           note "Feature tests done" &&
           python $PY_SUITE &&
