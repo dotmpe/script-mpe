b672e0442595591cf0be0e24ae0e0c6d8b17f6fb
Travis <dev+travis@dotmpe.com>
2018-03-17 03:39:05 +0100

    Testing

--- tools/ci/parts/announce.sh
+++ tools/ci/parts/announce.sh
@@ -8,15 +8,16 @@ curl -sSf https://$CI_DB_HOST/ || {
 }
 
 node --version
-# FIXME: nodejs needed, use curl instead
-  CI_DB_HOST="$CI_DB_HOST" \
-  CI_DB_INFO="$CI_DB_INFO" \
-  CI_DB_NAME='build-log' \
-      node ./tools/update-couchdb-testlog.js || {
+# FIXME: nodejs deps needed, use curl instead
 
-      echo "Ignored announcer failure" >&2
-      sleep 2
-      exit 0
-    }
+#  CI_DB_HOST="$CI_DB_HOST" \
+#  CI_DB_INFO="$CI_DB_INFO" \
+#  CI_DB_NAME='build-log' \
+#      node ./tools/update-couchdb-testlog.js || {
+#
+#      echo "Ignored announcer failure" >&2
+#      sleep 2
+#      exit 0
+#    }
 
 # Id: script-mpe/0.0.4-dev tools/ci/parts/announce.sh
