7be9f2056120d04fa09aa108f3dcd75d79ca6cf9
B. van Berkum <dev@dotmpe.com>
2018-06-17 20:16:56 +0200

    Build files

--- tools/ci/parts/install.sh
+++ tools/ci/parts/install.sh
@@ -11,6 +11,8 @@ test "$(whoami)" = "travis" || {
     {
       echo sudo=$sudo APT_PACKAGES=$APT_PACKAGES
       {
+        apt-get search apt-transport-https
+        apt-get search dig
         $sudo apt-get update &&
         $sudo apt-get install $APT_PACKAGES
 
