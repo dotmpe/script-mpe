e8743e881057076611d10d3d1a8c8de6da39be7a
B. van Berkum <dev@dotmpe.com>
2018-06-17 19:40:59 +0200

    Build files

--- tools/sh/box.env.sh
+++ tools/sh/box.env.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Something to manage messages
-test -n "$LOG" -a -x "$LOG" || exit 170
+#test -n "$LOG" -a -x "$LOG" || exit 170
 
 # Place to store Box files
 test -n "$BOX_DIR" || export BOX_DIR=$HOME/.box
@@ -12,7 +12,10 @@ test -n "$BOX_DIR" || export BOX_DIR=$HOME/.box
 test -n "$BOX_BIN_DIR" || export BOX_BIN_DIR=$BOX_DIR/frontend
 
 # Mark env or fail on reload
-test -z "$BOX_INIT" && BOX_INIT=1 || $LOG "box.env" error "unexpected re-init" 1
+test -z "$BOX_INIT" && BOX_INIT=1 || {
+    #$LOG "box.env" error "unexpected re-init" 1
+    echo "box.env" error "unexpected re-init" 1>&2 ; return 1
+}
 
 # run-time test since box relies on local vars and Bash seems to mess up
 box_run_sh_test()
