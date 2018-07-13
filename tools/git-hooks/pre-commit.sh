2de60870de7824f638e65d94c1f7e3ff9fe5379e
B. van Berkum <dev@dotmpe.com>
2018-07-13 16:57:50 +0200

    Poking at git-hooks a bit

--- tools/git-hooks/pre-commit.sh
+++ tools/git-hooks/pre-commit.sh
@@ -1,2 +1,6 @@
+# Executed before commit-msg is determined
+
+# See man 5 githooks
+
 test -z "$scm_nok" || exit $scm_nok ; echo "pre-commit: Set scm_nok= to override exit" >&2
 set -e ; pd run htd:gitflow-check-doc :verbose=1:vchk :bats:specs
---
+++ tools/git-hooks/prepare-commit-msg.sh
@@ -0,0 +1,24 @@
+# Executed after generating, before editing commit-msg
+
+# prepare-commit-msg <Msg-File> <Type> [<SHA-1>]
+
+# See man 5 githooks
+
+case "$2" in
+
+  message )
+    ;;
+
+  template )
+    ;;
+
+  merge )
+    ;;
+
+  squash )
+    ;;
+
+  commit )
+    ;;
+
+esac
