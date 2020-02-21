c9d6a6dc0a704c86353a85cc09aae8eed2f903f2
B. van Berkum <dev@dotmpe.com>
2020-02-21 22:05:09 +0100

    CI

--- tools/sh/parts/init-dependencies.sh
+++ tools/sh/parts/init-dependencies.sh
@@ -4,10 +4,8 @@
 # XXX: Get checkouts, tool installs and rebuild env (PATH etc.)
 VND_SRC_PREFIX=$HOME/build
 . ./tools/sh/parts/env-0-src.sh
-set -eo pipefail
 . $sh_tools/parts/init.sh
 $INIT_LOG "note" "" "Installing prerequisite repos" "$VND_SRC_PREFIX"
 init-deps dependencies.txt
-set -euo pipefail
 
 # Sync: U-S:
