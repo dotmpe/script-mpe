878210d8271c12302d82514186899b0ca3f256ed
B. van Berkum <dev@dotmpe.com>
2020-03-06 22:14:56 +0100

    Testing

--- tools/sh/parts/remove-dupes.sh
+++ tools/sh/parts/remove-dupes.sh
@@ -3,5 +3,5 @@ remove-dupes() # ~
 {
   awk '!a[$0]++'
 }
-type remove_dupes || alias remove_dupes=remove-dupes # FIXME: load proper file
+type remove_dupes >/dev/null 2>&1 || alias remove_dupes=remove-dupes # FIXME: load proper file
 # Id: U-S:tools/sh/parts/remove-dupes.sh                          vim:ft=bash:
