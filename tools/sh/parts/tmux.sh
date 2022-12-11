1b939e974dd74fa42f690b65a2646d4762aa36aa
B. van Berkum <dev@dotmpe.com>
2022-12-11 18:57:29 +0100

    t460s

--- tools/sh/parts/tmux.sh
+++ tools/sh/parts/tmux.sh
@@ -1,16 +1,19 @@
 
 alias tmux-options='tmux show-options -g'
 
+alias tmux-set-autolock='tmux set-option -g lock-after-time'
+
 alias tmux-toggle-autolock='{
 
   test -z "${tmux_lock_timeout:-}" && {
     tmux_lock_timeout=$(tmux show-option -gv lock-after-time)
     test -n "${tmux_lock_timeout:-}" || return
-    tmux set-option -gv lock-after-time 0
+    tmux set-option -g lock-after-time 0
     tmux display-message "Autolock timer OFF"
   } || {
-    tmux set-option -gv lock-after-time $tmux_lock_timeout
+    tmux set-option -g lock-after-time $tmux_lock_timeout
     tmux display-message "Autolock timer at ${tmux_lock_timeout:?} seconds"
+    unset tmux_lock_timeout
   }
 
 }'
