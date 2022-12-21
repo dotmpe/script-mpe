0d9616a194bab745903558264326284b8c340b06
B. van Berkum <dev@dotmpe.com>
2022-12-21 01:03:08 +0100

    [profile] Updated grep benchmark a bit

--- tools/benchmark/bm-sh-grep.sh
+++ tools/benchmark/bm-sh-grep.sh
@@ -1,19 +1,18 @@
 
 ### Comparing regex tools
 
-# Looking if GNU grep can be replaced. Only for one generic test currently.
+# Looking if GNU grep can be replaced.
 
-# Cannot get any benefit from ripgrep, sometimes for simple queries it
-# underperforms even almost two magnitudes. Silversearch is much worse also.
-#
-# Cannot get ripgrep benchsuite to run without diving into source, but looking
-# at a few of the provided benchmark summaries and by doing some local test
-# runs I don't see much speed benefit.
-
+# Ripgrep can perform better on large files. Not sure, it may be interesting
+# to compare the Linux src test with personal use cases. As for grepping ASCII
+# source scripts it looks like grep can easily out-do it, XXX: so need to try
+# and turn utf-8 off.
 
 source tools/benchmark/_lib.sh
 
-runs=1
+runs=10
+
+# Tried to get some results but there is too much noise currently on my dev.
 
 true "${testf:=htd.sh}"
 bre='^ *\(#.*\)\?$'
@@ -21,7 +20,12 @@ re='^ *(#.*)?$'
 
 test_1a_grep ()
 {
-  grep -v "$bre" "${testf:?}"
+  LC_ALL="en_US.utf-8" grep -v "$bre" "${testf:?}"
+}
+
+test_1a2_grep_C ()
+{
+  LC_ALL=C grep -v "$bre" "${testf:?}"
 }
 
 test_1b_egrep ()
@@ -29,57 +33,94 @@ test_1b_egrep ()
   grep -Ev "$re" "${testf:?}"
 }
 
-test_2_ripgrep ()
+test_1b2_egrep_C ()
+{
+  LC_ALL=C grep -Ev "$re" "${testf:?}"
+}
+
+test_2a_ripgrep ()
 {
   rg -v "$re" "${testf:?}"
 }
 
-test_2b_ripgrep ()
+test_2b1_ripgrep_C ()
+{
+  LC_ALL=C rg -v "$re" "${testf:?}"
+}
+
+test_2b2_ripgrep ()
 {
   rg -j1 -uuu -v "$re" "${testf:?}"
 }
 
+test_2b3_ripgrep ()
+{
+  rg -v "$re" "${testf:?}"
+}
+
+# XXX: tweak options/env?
 test_3_silversearcher ()
 {
   ag -v "$re" "${testf:?}"
 }
 
 
+## 1. grep
+
 echo "Grep ($(test_1a_grep | wc -l) lines)"
-time sh_null test_1a_grep
+#time sh_null test_1a_grep
 time sh_null run_test $runs 1a_grep
-time sh_null run_test 10 1a_grep
+#time sh_null run_test 10 1a_grep
+echo
+
+echo "Grep ($(test_1a2_grep_C | wc -l) lines)"
+#time sh_null test_1a2_grep_C
+time sh_null run_test $runs 1a2_grep_C
 echo
 
 echo "Grep -E ($(test_1b_egrep | wc -l) lines)"
-time sh_null test_1b_egrep
-#time sh_null run_test $runs 1b_egrep
+#time sh_null test_1b_egrep
+time sh_null run_test $runs 1b_egrep
 echo
 
-echo "Rg ($(test_2_ripgrep | wc -l) lines)"
-time sh_null test_2_ripgrep
-#time sh_null run_test $runs 2_ripgrep
+echo "Grep -E ($(test_1b2_egrep_C | wc -l) lines)"
+#time sh_null test_1b2_egrep_C
+time sh_null run_test $runs 1b2_egrep_C
 echo
 
-echo "Rg -j1 -uuu ($(test_2b_ripgrep | wc -l) lines)"
-time sh_null test_2b_ripgrep
-#time sh_null run_test $runs 2b_ripgrep
+
+## 2. ripgrep
+
+echo "Rg (C)($(test_2b1_ripgrep_C | wc -l) lines)"
+#time sh_null test_2b1_ripgrep_C
+time sh_null run_test $runs 2b1_ripgrep_C
 echo
+
+echo "Rg -j1 -uuu ($(test_2b2_ripgrep | wc -l) lines)"
+#time sh_null test_2b2_ripgrep
+time sh_null run_test $runs 2b2_ripgrep
+echo
+
+echo "Rg ($(test_2b3_ripgrep | wc -l) lines)"
+#time sh_null test_2b3_ripgrep
+time sh_null run_test $runs 2b3_ripgrep
+echo
+
 
 #echo "Ag ($(test_3_silversearcher | wc -l) lines)"
 #time sh_null run_test $runs 3_silversearcher
 #echo
 
-re='\w+\s+Холмс\s+\w+'
-
-echo "2. Grep -E ($(test_1b_egrep | wc -l) lines)"
-time sh_null test_1b_egrep
-#time sh_null run_test $runs 1b_egrep
-echo
-
-echo "2. Rg ($(test_2_ripgrep | wc -l) lines)"
-time sh_null test_2_ripgrep
-#time sh_null run_test $runs 2_ripgrep
-echo
+#re='\w+\s+Холмс\s+\w+'
+#
+#echo "2. Grep -E ($(test_1b_egrep | wc -l) lines)"
+#time sh_null test_1b_egrep
+##time sh_null run_test $runs 1b_egrep
+#echo
+#
+#echo "2. Rg ($(test_2_ripgrep | wc -l) lines)"
+#time sh_null test_2_ripgrep
+##time sh_null run_test $runs 2_ripgrep
+#echo
 
 #
