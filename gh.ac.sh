20bbe1d3c301f3db14d09010d1b47a260fe7c986
B. van Berkum <dev@dotmpe.com>
2023-01-12 19:27:48 +0100

    t470p

--- gh.ac.sh
+++ gh.ac.sh
@@ -2,10 +2,12 @@
 ### Auto-compile and source AC for github CLI, update once command file changes
 true "${SHELL_NAME:=$(basename "${SHELL/-}")}"
 true "${GH_BIN:=$(command -v gh)}"
-true "${GH_AC_SH:="${PROJECT_CACHE:-.meta/cache}/gh.ac.sh"}"
-{
-  test -e "$_" -a "$GH_BIN" -nt "$_"
-} || {
-  gh completion -s ${SHELL_NAME:?} >| "$_"
+test -z "$GH_BIN" || {
+  true "${GH_AC_SH:="${PROJECT_CACHE:-.meta/cache}/gh.ac.sh"}"
+  {
+    test -e "$_" -a "$GH_BIN" -nt "$_"
+  } || {
+    gh completion -s ${SHELL_NAME:?} >| "$_"
+  }
+  . "${GH_AC_SH:?}"
 }
-. "${GH_AC_SH:?}"
