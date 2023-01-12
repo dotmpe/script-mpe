37b5af52d6977de9cd76b6fe92effaa18ecb24aa
B. van Berkum <dev@dotmpe.com>
2023-01-12 09:07:24 +0100

    t460s

--- bat-if.sh
+++ bat-if.sh
@@ -11,7 +11,9 @@ true "${LINES:=$(tput lines)}"
 args=${@:-/dev/stdin}
 #echo "bat-if pager reading (from) '$args'" >&2
 data=$(<"$args")
-lines=$(echo "$data" | wc -l)
+test -z "$data" &&
+    lines=0 ||
+    lines=$(echo "$data" | wc -l)
 # Set either USER_LINES or UC_OUTPUT_LINES in profile to page on more or less
 # lines
 maxlines=${USER_LINES:-${UC_OUTPUT_LINES:-${LINES:?}}}
@@ -21,10 +23,17 @@ test $maxlines -le $lines && {
       echo "bat-if read $lines lines, max inline output is $maxlines" >&2
   bat_opts=--paging=always\ --style=rule,numbers
 } || {
-  bat_opts=--paging=never\ --style=grid,numbers
+  # Display 'File: ... <EMPTY>' (without deco) even if there is no content
+  # but only if quiet_empty=false (see below)
+  test $lines -eq 0 &&
+      bat_opts=--paging=never\ --style=plain ||
+      bat_opts=--paging=never\ --style=grid,numbers
 }
 
-test "$args" = /dev/stdin || bat_opts=$bat_opts,header\ --file-name="$args"
+# Display 'File:' header for both paging and nonpaging if known
+{ ${quiet_empty:-true} || test $lines -gt 0
+} && test "$args" = /dev/stdin ||
+    bat_opts=$bat_opts,header\ --file-name="$args"
 
 
 # Careful: if PAGER is set to this script, IF_PAGER must be set as well
@@ -34,5 +43,5 @@ test -z "${IF_PAGER:-}" || {
     export PAGER=less
 }
 
-echo "$data" | ${IF_PAGER:-${PAGER:?}} $bat_opts
+printf %s "$data" | ${IF_PAGER:-${PAGER:?}} $bat_opts
 #
