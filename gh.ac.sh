c5ae2f61de1822eba28e21380e4d9da6b7cde2ea
B. van Berkum <dev@dotmpe.com>
2023-01-17 03:38:07 +0100

    t460s

--- gh.ac.sh
+++ gh.ac.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 ### Auto-compile and source AC for github CLI, update once command file changes
 true "${SHELL_NAME:=$(basename "${SHELL/-}")}"
+true "${US_BIN:=$HOME/bin}"
 true "${GH_BIN:=$(command -v gh)}"
+# Ignore if gh is not installed
 test -z "$GH_BIN" || {
   true "${GH_AC_SH:="${PROJECT_CACHE:-.meta/cache}/gh.ac.sh"}"
   {
-    test -e "$_" -a "$GH_BIN" -nt "$_"
+    test -e "$US_BIN/$_" -a "$GH_BIN" -nt "$US_BIN/$_"
   } || {
     gh completion -s ${SHELL_NAME:?} >| "$_"
   }
-  . "${GH_AC_SH:?}"
+  . "$US_BIN/${GH_AC_SH:?}"
 }
