61fda33c5c21257cdd0a33cd5f3a5f0f27234a21
B. van Berkum <dev@dotmpe.com>
2023-03-30 20:55:51 +0200

    t470p

---
+++ usb.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s nullglob
+
+info ()
+{
+  for x in /sys/bus/usb/devices/${1:?}/
+  do
+    false
+  done
+}
+
+list ()
+{
+  for usbnum in /sys/bus/usb/devices/usb[0-9]*/busnum
+  do
+    busnum=$(< "$usbnum")
+    device=$(dirname "$usbnum")
+    usbdev=$(< "$device/dev")
+    usbver=$(< "$device/version")
+    usbsp=$(< "$device/speed")
+    test ! -e "$device/product" && usbprod= || usbprod=$(< "$device/product")
+    echo "USB bus $busnum $usbprod (version $usbver, speed $usbsp, device $usbdev)"
+
+    for usbnum2 in $device/*/busnum
+    do
+      device2=$(dirname "$usbnum2")
+      usbdev=$(< "$device2/dev")
+      usbver=$(< "$device2/version")
+      usbsp=$(< "$device2/speed")
+      test ! -e "$device2/product" && usbprod= || usbprod=$(< "$device2/product")
+      echo "  $(basename $device2) $usbdev $usbver $usbsp $usbprod"
+    done
+  done
+}
+
+on ()
+{
+  echo "${1:?}" |
+      sudo -p "Password to enable USB port '${1:?}' (%u->%U): " \
+      tee /sys/bus/usb/drivers/usb/bind
+}
+
+off ()
+{
+  echo "${1:?}" |
+      sudo -p "Password to disable USB port '${1:?}' (%u->%U): " \
+      tee /sys/bus/usb/drivers/usb/unbind
+}
+
+"$@"
