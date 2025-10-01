14a278d91b1157534a0cf3544f6722236ef9f9c6
B. van Berkum <dev@dotmpe.com>
2025-10-01 12:08:16 +0200

    Accumulated at t470p

--- date-htd.lib.sh
+++ date-htd.lib.sh
@@ -277,12 +277,12 @@ fmttime_relative_f ()
     test -n "${2:-}" && shift ||
       set -- $(echo "scale=24; $(epoch_microtime) - $1"|bc) "$3"
     test "${1:0:1}" != "-" ||
-      stderr_ "! $0: seconds-fmt-relative-f input ts was before epoch" $? || return
+      failwith "! $0: seconds-fmt-relative-f input ts was before epoch" $? || return
   }
   test -n "${1:-}" -a $# -le 2 || return 64
   test -n "${2:-}" || set -- "$1" ""
   test ${1:0:1} != "-" ||
-    stderr_ "! $0: seconds-fmt-relative-f takes only positive delta values" $? || return
+    failwith "! $0: seconds-fmt-relative-f takes only positive delta values" $? || return
 
   test ${1//.*} -gt 0 && {
     # Seconds
