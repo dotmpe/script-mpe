9bb7a887e6767a950ed72b959001c6cf1e4df303
B. van Berkum <dev@dotmpe.com>
2025-10-01 12:03:08 +0200

    Accumulated at t470p

--- tool/sh/part/als-git.sh
+++ tool/sh/part/als-git.sh
@@ -269,16 +269,16 @@ alias :quiet=std_quiet
 alias :silent=std_silent
 alias :git:push:all='for d in ~/{.l/c,.conf,bin,{project/user-{conf,scripts}}}
 do
-  :noo pushd $d || continue
+  std_noo pushd $d || continue
   :gcn && :gs
-  :noo popd
+  std_noo popd
 done
 '
 alias :git:sync:all='for d in ~/{.l/c,.conf,bin,{project/user-{conf,scripts}}}
 do
-  :noo pushd $d || continue
+  std_noo pushd $d || continue
   :gs
-  :noo popd
+  std_noo popd
 done
 '
 alias :git:update=':git:fetch:v --all && :gpa'
