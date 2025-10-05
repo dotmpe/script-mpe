48319d9fb49d810dc7936590f3bc1a5533a6763d
B. van Berkum <dev@dotmpe.com>
2025-10-05 07:38:19 +0200

    Accumulated at t470p

--- srv.sh
+++ srv.sh
@@ -1,110 +1,28 @@
-#!/usr/bin/env make.sh
-# Created: 2017-02-25
+#!/usr/bin/env bash
 
-version=0.0.4-dev # script-mpe
+#lib_require srv
+#srv__update_services
 
+user=${SUDO_USER:-${USER?}}
 
-# Script subcmd's funcs and vars
+mount_basedirs=( /media/${user?} /run/media/${user?} /mnt/nfs /mnt )
 
-# See '$scriptname help' to get started
+. "common,cache,uc.sh"
 
-srv_man_1__list="List all service volume instances"
-srv__list()
-{
-  # Get service names and locations from DB, error if paths are missing
-  true
-}
-srv__global_status=list
+cache_loadmaps "${STATUSDIR_ROOT:-$HOME/.local/var/statusdir/}user-srv.sh" \
+  volume_ids
 
-
-srv_spc__list_raw='list-raw [GLOB [FMT]]'
-srv__list_raw()
-{
-  true
-}
-
-
-srv_man_1__info="Quick stats.."
-srv__info()
-{
-  # Give info on service volume instance, or generic stats
-  true
-}
-
-
-srv_man_1__status="Semi quick stats.."
-srv__status()
-{
-  # Recompile staging, by running update for our contexts w/o commit
-  srv__update_services
-  # Final comment on changes in staging
-  true
-}
-
-
-srv_man_1__info_raw="Update cache and give parsed details for local ..."
-srv_spc__info_raw=" GREP [ FMT ] "
-srv__info_raw()
-{
-  echo
-}
-
-
-srv_man_1__list_info="Update and list actual info about instances"
-srv__list_info()
-{
-  echo
-}
-srv_als__details=list-info
-srv_als__update=list-info
-
-
-srv_man_1__update_services='Run both a check on the index and actual names found. '
-srv__update_services()
-{
-  # Get service names and locations from DB and see wether paths exist and are
-  # targetted correctly, or update
-  true
-  # Walk over service names in FS roots, check wether they are tracked as
-  # service or prepare an entry
-  true
-  # Report on all services in staging, commit if required
-  true
-}
-
-
-
-
-# Generic subcmd's
-
-srv_als____version=version
-srv_als___V=version
-srv_grp__version=ctx-main\ ctx-std
-
-srv_als____help=help
-srv_als___h=help
-srv_grp__help=ctx-main\ ctx-std
-
-
-srv__edit()
-{
-  $EDITOR $0 $(which $base.py) "$@"
-}
-srv_als___e=edit
-
-
-## Main parts
-
-MAKE-HERE
-INIT_ENV="init-log 0 0-src 0-u_s 0-1-lib-sys 0-std ucache scriptpath box"
-INIT_LIB="\\$default_lib main meta box doc date table remote std stdio"
-main-local
-failed= dry_run=
-main-lib
-  local lib_loading=1
-  INIT_LOG=${LOG:?} lib_init || return
-main-unload
-  clean_failed || unload_ret=$?
-  unset failed
-main-epilogue
-# Id: script-mpe/0.0.4-dev srv.sh
+for volid in "${!volume_ids[@]}"
+do
+  [[ -h /srv/"$volid" ]] &&
+  [[ -e /srv/"$volid"/ ]] && continue
+  [[ -h /srv/"$volid" ]] && rm -v /srv/"$volid"
+  for volname in ${volume_ids["$volid"]}
+  do
+    for mountdir in "${mount_basedirs[@]}"
+    do
+      [[ -e "$mountdir/$volname" ]] || continue
+      ln -vs "$mountdir/$volname" /srv/$volid
+    done
+  done
+done
