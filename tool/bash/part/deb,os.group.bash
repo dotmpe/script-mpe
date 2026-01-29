979fee8152bc0e06757c6ca47cfedb100b185ee7
B. van Berkum <dev@dotmpe.com>
2026-01-29 11:56:40 +0100

    @t470p

--- tool/bash/part/deb,os.group.bash
+++ tool/bash/part/deb,os.group.bash
@@ -3,11 +3,10 @@ os_deb_ssc=(
 
 # Fetch updates, upgrade and remove+clean
 [debian.cycle]=debian.os.update
+[debian.dpkg.is-installed]='dpkg -s "$@"'
+[debian.nvidia.upgrade]='sudo apt purge *nvidia* && sudo apt install linux-headers-$(uname -r) && sudo apt install -y nvidia-driver'
 [debian.os.clean-packages]='sudo apt-get autoremove -y && sudo apt-get autoclean'
-
 [debian.os.update]='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y && sudo apt-get autoclean'
 [debian.os.upgrade]='debian.os.update && sudo apt dist-upgrade && debian.os.clean-packages'
-
-[debian.dpkg.is-installed]='dpkg -s "$@"'
 )
 #
