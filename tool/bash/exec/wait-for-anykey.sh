9e33bf63b85330e3368b21ae62f541b4cd9fea79
B. van Berkum <dev@dotmpe.com>
2026-02-12 01:01:44 +0100

    @t460s

--- tool/bash/exec/wait-for-anykey.sh
+++ tool/bash/exec/wait-for-anykey.sh
@@ -3,6 +3,12 @@
 set -euETo pipefail
 trap - SIGINT
 
+: "${_c2:=$(tput setaf 2)}"
+: "${_c3:=$(tput setaf 3)}"
+: "${_c6:=$(tput setaf 6)}"
+: "${_c15:=$(tput setaf 15)}"
+: "${NORMAL:=$(tput sgr0)}"
+
 : "${restart_delay:=10}"
 
 test-int ()
@@ -24,23 +30,29 @@ catch_sleep ()
 
 : "${run:=1}"
 stat=0
+wait=1
 while true
 do
   ! ((run)) || {
-    clear
-    read -r -p "░░░ Enter  any key  to activate terminal " -n 1
+    ! ((wait)) || {
+      clear
+      read -r -p "${_c2}░░░ Enter ${_c6} ${_c15}any key ${_c6} ${_c2}to activate terminal${NORMAL} " -n 1
+    }
     "$@" && stat=$? || stat=$?
-    echo "▒  Command '$*' ended E$stat"
+    echo "${_c2}▒   Command '$*' ended E$stat${NORMAL}"
   }
 
   ! ((stat)) || {
-    echo "▓   Command exited E$?, $0 will restart in $restart_delay seconds, interrupt to abort"
+    echo "${_c2}▓   Command exited ${_c3}E$?${_c2}, $0 will restart in $restart_delay seconds, interrupt to abort${NORMAL}"
     catch_sleep $restart_delay && continue || run=0 stat=0
   }
 
-  echo "█   Command completed: '$*'. Press 'r' to restart ${0##*/} COMMAND', and 'x' or interrupt to exit"
-  read -r -s -N 1 prompt
-  [[ $prompt == x ]] && exit ||
-  [[ $prompt == r ]] && run=1 || run=0
+  echo "${_c2}█   Command completed: '$*'. Press 'R' to reset, 'r' to restart ${0##*/} COMMAND now, and 'x' or interrupt to exit${NORMAL}"
+  read -r -s -N 1 prompt &&
+  [[ ${prompt-} != x ]] || exit 0
+
+  [[ ${prompt-} == R ]] && run=1 wait=1 || {
+    [[ ${prompt-} == r ]] && run=1 wait=0 || run=0
+  }
 done
 
