90c64b8783f994355afa914254536dbc0549e804
B. van Berkum <dev@dotmpe.com>
2026-02-15 08:44:46 +0100

    @x220

--- tool/bash/part/deb,os.group.bash
+++ tool/bash/part/deb,os.group.bash
@@ -4,6 +4,7 @@ os_deb_ssc=(
 # Fetch updates, upgrade and remove+clean
 [debian.cycle]=debian.os.update
 [debian.dpkg.is-installed]='dpkg -s "$@"'
+[debian.modified]='locate -ibe "*.dpkg-dist"'
 [debian.nvidia.upgrade]='sudo apt purge *nvidia* && sudo apt install linux-headers-$(uname -r) && sudo apt install -y nvidia-driver && dkms status'
 [debian.add]='sudo apt install -qqy'
 [debian.os.refresh]='sudo apt update -qq'
@@ -12,6 +13,7 @@ os_deb_ssc=(
 [debian.os.clean-cache]='sudo apt-get autoclean'
 [debian.os.update]='debian.os.refresh && sudo apt-get upgrade -qqy && debian.os.clean'
 [debian.os.upgrade]='debian.os.update && sudo apt dist-upgrade && debian.os.clean'
+[debian.remove]='sudo apt remove --purge -qq'
 
 )
 # Id: debian,os                                  vim:set ft=bash sw=2 sts=2 et:
